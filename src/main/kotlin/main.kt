6c213c309ac201a6bf55d7a937b7d892bf91f304
Ciaran Bent <ciaran.bent@protonmail.ch>
2021-05-02 21:25:02 +0100

    Started Application Layout Creation; Added Licensing;
    - Diagrams/Layouts/MainInterface.drawio
      - Added diagram to help planning the Main interface layout.
    
    - src/main/kotlin/main.kt
      - Added first draft of layout (subject to change).
      - Moved Welcome screen specific code into its own file.
    
    - src/main/kotlin/screens/DisplayState.kt
      - Added Enum for tracking the current screen to be displayed.
    
    - src/main/kotlin/screens/filepicker/FilePicker.kt
      - Added placeholder for File Picking screen.
    
    - src/main/kotlin/screens/welcome/Welcome.kt
      - New file to handling displaying Welcome specific screen.
    
    - src/main/kotlin/util/CustomStyle.kt
      - Added Kotlin object to handle custom style reuse.
    
    - src/main/kotlin/util/Resource.kt
      - Renamed Resources file to Resource.
      - Corrected issue which would have been problematic if two keys
        started with the same text.  This is no longer a problem.
    
    - src/main/resources/strings/common-strings.properties
      - Added new String text.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,92 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Ciaran Bent
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 import androidx.compose.desktop.Window
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-
-import util.Resources
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import screens.DisplayState
+import screens.filepicker.filePickerScreen
+import screens.welcome.welcomeScreen
+import util.CustomStyle
+import util.Resource
 
 fun main() = Window(
-    title = Resources.getStringResource("title-text")
+    title = Resource.getStringResource("title-text")
 ) {
-    var text by remember { mutableStateOf("Hello, World!") }
+    // State management for the application
+    var applicationState = remember { mutableStateOf(DisplayState.Welcome) }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    // Styling
+    val padValue: Dp = 16.dp
+
+    Card(
+        Modifier
+            .fillMaxSize()
+            .padding(padValue)
+    ) {
+        Column {
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                // Title Text which stays constant throughout application
+                Text(
+                    text = Resource.getStringResource("application-header"),
+                    style = CustomStyle.titleText()
+                )
+            }
+
+            Spacer(Modifier.height(padValue))
+
+            Row {
+                Card {
+                    // Interactive part of the application
+                    Row {
+                        when (applicationState.value) {
+                            DisplayState.Welcome -> {
+                                welcomeScreen(applicationState)
+                            }
+                            DisplayState.FilePicker -> {
+                                filePickerScreen(applicationState)
+                            }
+                            DisplayState.EditFile -> TODO()
+                        }
+                    }
+                }
+            }
         }
     }
 }
